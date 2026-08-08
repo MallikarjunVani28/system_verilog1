0e548f122e72708faf5efa0293148705b0054cb3
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-08-08 22:14:48 +0530

    SystemVerilog class and testbench for randomization

---
+++ 05.Constraint/11.randomize.sv
@@ -0,0 +1,23 @@
+//Randomize variables where:data != addr
+class example;
+rand int a;
+rand int b;
+constraint c{ a != b ;
+}
+constraint siz{ a inside {[1:100]};}
+constraint sma{ b inside {[1:100]};}
+endclass
+
+module tb;
+example ex;
+
+initial begin
+ex = new();
+repeat(4)begin
+if(ex.randomize())
+$display("a = %d b = %d ",ex.a,ex.b);
+else
+$display("failed");
+end
+end
+endmodule  
