c6e14f5f2aca76a86020979c731009222b13cfae
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-08-08 15:19:50 +0530

    SystemVerilog code for random even number generation

---
+++ 05.Constraint/06.random_even.sv
@@ -0,0 +1,28 @@
+// sv code to generate the random numbers
+
+class example;
+  rand int a[10];
+  
+  constraint div{ foreach(a[i])
+  a[i] % 2 == 0;}
+
+constraint range{foreach(a[i])
+ a[i] inside {[1:100]};}
+endclass
+
+module tb;
+  
+  example ex;
+  initial begin
+    
+  ex = new;
+    
+    if(ex.randomize())
+      $display("%p",ex.a);
+    else
+      $display("randomization failed");
+  end
+endmodule
+  
+  
+  
