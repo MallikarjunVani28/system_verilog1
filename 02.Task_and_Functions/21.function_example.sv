b0119831c0d31baa2227c089cc4cd7ef3434e209
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-08-20 11:48:39 +0530

    predict the output of the code

---
+++ 02.Task_and_Functions/21.function_example.sv
@@ -0,0 +1,48 @@
+module test;
+  int a=2,b=3;
+  int d , result;
+  
+  function automatic  mult( ref int a, ref int b, output int c);
+    c = (a*b)+2;
+    a++;
+    $display("inside the function a = %0d b = %0d c = %0d",a,b,c);
+  endfunction
+  
+  initial 
+    fork
+      begin
+        #1;
+        mult(a,b,d);
+        $display("outside the function  at time = %0d a = %0d b = %0d c = %0d",$time,a,b,d);
+      end
+      begin
+      #2;
+        mult(a,b,d);
+        $display("outside the function  at time = %0d a = %0d b = %0d c = %0d",$time,a,b,d);
+      end
+    join
+  
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+inside the function a = 3 b = 3 c = 8
+outside the function  at time = 1 a = 3 b = 3 c = 8
+inside the function a = 4 b = 3 c = 11
+outside the function  at time = 2 a = 4 b = 3 c = 11
+*/
+      
+     
+        
+        
+    
