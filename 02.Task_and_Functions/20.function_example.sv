cb3ee728fb4df1eadc31595d613287d955c53de5
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-08-20 11:39:12 +0530

    function example for multiplication in SystemVerilog

---
+++ 02.Task_and_Functions/20.function_example.sv
@@ -0,0 +1,47 @@
+module test;
+  int a=2,b=3;
+  int d , result;
+  
+  function automatic  mult( input int a, input int b, output int c);
+    c = (a*b)+2;
+    a++;
+    $display("inside the function a = %0d b = %0d c = %0d",a,b,c);
+  endfunction
+  
+  initial 
+    fork
+      begin
+        #1;
+        mult(2,3,d);
+        $display("outside the function  at time = %0d a = %0d b = %0d c = %0d",$time,a,b,d);
+      end
+      begin
+      #2;
+        mult(2,4,d);
+        $display("outside the function  at time = %0d a = %0d b = %0d c = %0d",$time,a,b,d);
+      end
+    join
+  
+endmodule
+
+
+
+
+
+
+
+
+
+
+/* output
+inside the function a = 3 b = 3 c = 8
+outside the function  at time = 1 a = 2 b = 3 c = 8
+inside the function a = 3 b = 4 c = 10
+outside the function  at time = 2 a = 2 b = 3 c = 10
+*/
+      
+      
+     
+        
+        
+    
