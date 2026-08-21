76f2275a61b1d39ccf667969b4072bc226b6cdb8
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-08-21 08:07:05 +0530

    pass by reference example in SystemVerilog
    Demonstrate pass by reference in SystemVerilog.

---
+++ 02.Task_and_Functions/27.pass_by_ref.sv
@@ -0,0 +1,17 @@
+module tb;
+  function void modify(ref int a[5]);
+    a[0] = 100;
+    $display("inside the function a = %p",a);
+  endfunction
+  
+  initial begin
+    int a[5] = '{1,2,3,4,5};
+    modify(a);
+    $display("outside the function a = %p",a);
+  end
+endmodule
+
+/*
+inside the function a = '{'h64, 'h2, 'h3, 'h4, 'h5}
+outside the function a = '{'h64, 'h2, 'h3, 'h4, 'h5}
+*/
