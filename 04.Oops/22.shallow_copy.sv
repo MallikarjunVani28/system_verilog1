a53d22818febe45dd008a6fdf36f85279c181437
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-08-23 09:40:17 +0530

    shallow copy example in SystemVerilog
    This code demonstrates shallow copy behavior in SystemVerilog by assigning one object to another and modifying its property.

---
+++ 04.Oops/22.shallow_copy.sv
@@ -0,0 +1,34 @@
+/*Create a class:
+
+class student;
+  int roll;
+  int marks;
+endclass
+
+Create s1 and s2.
+
+Assign:
+
+s2 = s1;
+
+Modify s2.marks.
+
+Observe s1.marks.*/
+class student;
+  int roll_num;
+  int marks;
+endclass
+
+module tb;
+  student s1,s2;
+  initial begin
+    s1 = new();
+    s1.roll_num  = 12;
+    s1.marks = 88;
+    
+    s2 = s1;
+    s2.marks = 89;
+    
+    $display("s1 = %p and s2 = %p",s1,s2);
+  
+endmodule
