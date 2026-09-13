b1060eb01048f5c460c748955465a6f335075715
Mallikarjun Vani <149805951+MallikarjunVani28@users.noreply.github.com>
2026-09-13 18:10:14 +0530

    packet and generator classes for simulation

--- 09.predict_output/05.predict_output.sv
+++ 09.predict_output/05.predict_output.sv
@@ -1,1 +1,45 @@
 
+class packet;
+  static int ID = 0;
+  int aid = 0;
+
+  function new();
+    ID++;
+    aid++;
+    $display("ID = %0d \t aid = %0d", ID, aid);
+  endfunction
+endclass : packet
+
+
+class generator;
+
+  task start();
+    packet pkt_h = new();
+    $display("%0d", pkt_h);
+  endtask : start
+
+endclass : generator
+
+
+module sp_test();
+
+  generator gen[4];
+
+  initial begin
+
+    foreach (gen[i]) begin
+      gen[i] = new;
+      $display(gen[i]);
+    end
+
+    gen[0].start();
+    gen[1].start();
+
+    repeat(10)
+      gen[2].start();
+
+    gen[3].start();
+
+  end
+
+endmodule
